94380efd9e82a1d5a1f4be33775335579d9c8280
sirnao <75645484+sirnao@users.noreply.github.com>
2024-02-14 22:06:56 -0100

    Basic Setup

---
+++ components/custom/navigation/index.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import { ModeToggle } from "../darkModeToggle"
+
+export function NavBar() {
+    return (
+       
+        <main className="fixed w-full h-20 top-8 flex items-center justify-center">
+            <div className="relative flex items-center w-11/12 h-full bg-popover border rounded-full">
+                <div className="absolute block right-3">
+                    <ModeToggle />
+                </div>
+            </div>
+        </main>
+        
+    )
+}
