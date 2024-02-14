21b937cb9f3b5fd002ed317d34417af1629bf123
sirnao <75645484+sirnao@users.noreply.github.com>
2024-02-14 22:37:52 -0100

    First sketch of main page

--- app/page.tsx
+++ app/page.tsx
@@ -6,15 +6,18 @@ export default function Home() {
     return (
 		<>
 			<NavBar />
-			<main className="absolute block top-40 left-0 w-full h-full min-h-screen">
+			<main className="absolute flex justify-center items-center left-0 w-full h-full min-h-screen">
 			
 
-				<h1>Noah Schittenhelm</h1>
-				<h2>Student</h2>
+				<div className="relative">
+					<h1 className="font-semibold text-3xl md:text-5xl xl:text-6xl uppercase      dark:bg-[linear-gradient(to_top_left,#7DE2FC,#B9B6E5)] dark:bg-clip-text dark:text-transparent">Noah Schittenhelm</h1>
+					<h2 className="py-5 font-regular text-md md:text-xl xl:text-2xl uppercase text-muted-foreground text-center">Student</h2>
+				
 
-				<div>
-					<Button>Portfolio</Button>
-					<Button variant="outline">Resume</Button>
+					<div className="w-full relative flex justify-center mt-5">
+						<Button className="uppercase mx-2 rounded-xl" variant="default">Portfolio</Button>
+						<Button className="uppercase mx-2 rounded-xl" variant="default">Resume</Button>
+					</div>
 				</div>
 
 			</main>
--- components/custom/navigation/index.tsx
+++ components/custom/navigation/index.tsx
@@ -4,8 +4,8 @@ import { ModeToggle } from "../darkModeToggle"
 
 export function NavBar() {
     return (
-       
-        <main className="fixed w-full h-20 top-8 flex items-center justify-center">
+        
+        <main className="fixed w-full h-20 top-8 flex items-center justify-center z-10">
             <div className="relative flex items-center w-11/12 h-full bg-popover border rounded-full">
                 <div className="absolute block right-3">
                     <ModeToggle />
